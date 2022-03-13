39a179587efa311051000a63ddc435d8ff62abc9
jimbosl1ce <jdhogerty@gmail.com>
2022-03-12 16:00:10 -0800

    Somewhat completed the results text

--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet, Pressable, Image } from 'react-native';
+import React, {useState} from 'react';
+import { Button, View, Text, StyleSheet, Pressable, Image, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function ResultsScreen({ navigation, route }) {
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
   const { quizScore } = route.params;
   return (
     <View style={styles.container}>
+
 
       <View style={styles.accountContainer}>
         <Pressable style={styles.accountPress}>
@@ -29,6 +33,25 @@ function ResultsScreen({ navigation, route }) {
           <Text style={styles.regularFont}>If you need some help, use our coffee quiz to learn how to make it better next time.</Text>
         </View>
       )}
+<ScrollView>
+      <View style={styles.tryThisContainer}>
+        <Pressable style={styles.tryThisRow} onPress={() => setOpen1(!open1)}>
+          <Text style={styles.tryThisText}>Grind Finer</Text>
+          <Image style={styles.tryThisIcon} source={require('../../assets/TryThisPlus.png')}/>
+        </Pressable>
+          {open1 ? (<Text style={styles.brewBetterText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</Text>) : (<></>)}
+        <Pressable style={styles.tryThisRow} onPress={() => setOpen2(!open2)}>
+          <Text style={styles.tryThisText}>Brew More Slowly</Text>
+          <Image style={styles.tryThisIcon} source={require('../../assets/TryThisPlus.png')}/>
+        </Pressable>
+        {open2 ? (<Text style={styles.brewBetterText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</Text>) : (<></>)}
+        <Pressable style={styles.tryThisRow} onPress={() => setOpen3(!open3)}>
+          <Text style={styles.tryThisText}>Use Hotter Water</Text>
+          <Image style={styles.tryThisIcon} source={require('../../assets/TryThisPlus.png')}/>
+        </Pressable>
+        {open3 ? (<Text style={styles.brewBetterText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</Text>) : (<></>)}
+      </View>
+      </ScrollView>
 
       <View style={styles.actionContainer}>
         <Pressable style={styles.startBrewing} onPress={()=> navigation.navigate('Home')}>
@@ -59,7 +82,12 @@ const styles = StyleSheet.create({
     // borderWidth: 1,
     marginBottom: 30
   },
-  actionContainer: {},
+  actionContainer: {
+    position: 'absolute',
+    width: '75%',
+    top: '97%',
+    right: '25%',
+  },
   startBrewing: {
     display: 'flex',
     alignItems: 'center',
@@ -100,6 +128,35 @@ const styles = StyleSheet.create({
   },
   font: {
     color: '#fff'
+  },
+  tryThisContainer: {
+    display: 'flex'
+  },
+  tryThisRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderTopColor: 'rgba(64, 63, 63, 1)',
+    borderTopWidth: 1,
+  },
+  tryThisText: {
+    color:"rgba(255, 255, 255, 1)",
+    fontSize: 22
+  },
+  tryThisIcon: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 14,
+    width: 14
+  },
+  brewBetterText: {
+    fontSize: 16,
+    color: '#666666',
+    marginBottom: 20,
+    paddingBottom: 50,
   }
 })
 
